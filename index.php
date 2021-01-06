2039fd50de829806d543247134acb2239e24d9a6
Bohdan Tarasiuk <bodik2836@gmail.com>
2021-01-06 17:47:59 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,62 @@
+<?php
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Factory\AppFactory;
+use Twig\Loader\FilesystemLoader;
+use Twig\Environment;
+use Blog\PostMapper;
+use Blog\LatestPosts;
+
+require __DIR__ . '/vendor/autoload.php';
+
+$loader = new FilesystemLoader('templates');
+$view = new Environment($loader);
+
+$config = include 'config/database.php';
+$dsn = $config['dsn'];
+$username = $config['username'];
+$password = $config['password'];
+
+try {
+    $connection = new PDO($dsn, $username, $password);
+    $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $connection->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+} catch (PDOException $exception) {
+    die('Database error: ' . $exception->getMessage());
+}
+
+$app = AppFactory::create();
+
+$app->get('/', function (Request $request, Response $response) use ($view, $connection) {
+    $latestPosts = new LatestPosts($connection);
+    $posts = $latestPosts->get(3);
+    $body = $view->render('index.twig', [
+        'posts' => $posts
+    ]);
+    $response->getBody()->write($body);
+    return $response;
+});
+
+$app->get('/about', function (Request $request, Response $response) use ($view) {
+    $body = $view->render('about.twig', ['name' => 'Bohdan']);
+    $response->getBody()->write($body);
+    return $response;
+});
+
+$app->get('/{url_key}', function (Request $request, Response $response, $args) use ($view, $connection) {
+    $postMapper = new PostMapper($connection);
+    $post = $postMapper->getByUrlKey((string) $args['url_key']);
+
+    if (empty($post)) {
+        $body = $view->render('not-found.twig');
+    } else {
+        $body = $view->render('post.twig', [
+            'post' => $post
+        ]);
+    }
+
+    $response->getBody()->write($body);
+    return $response;
+});
+
+$app->run();
